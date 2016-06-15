9e0fea3156ddf4c0f2fbf1403e640ccd23c954ed
warreee <ward.schodts@student.kuleuven.be>
2016-06-15 23:03:30 +0200

    Started on classification.

--- Assignment2/Classification/src/main/java/de/tuberlin/dima/aim3/assignment2/Classification.java
+++ Assignment2/Classification/src/main/java/de/tuberlin/dima/aim3/assignment2/Classification.java
@@ -1,17 +1,17 @@
 /**
  * AIM3 - Scalable Data Mining -  course work
  * Copyright (C) 2014  Sebastian Schelter, Christoph Boden
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -30,76 +30,83 @@ import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.FileSystem;
 
+import java.util.List;
 import java.util.Map;
 
 public class Classification {
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-    ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-    DataSource<String> conditionalInput = env.readTextFile(Config.pathToConditionals());
-    DataSource<String> sumInput = env.readTextFile(Config.pathToSums());
+        DataSource<String> conditionalInput = env.readTextFile(Config.pathToConditionals());
+        DataSource<String> sumInput = env.readTextFile(Config.pathToSums());
 
-    DataSet<Tuple3<String, String, Long>> conditionals = conditionalInput.map(new ConditionalReader());
-    DataSet<Tuple2<String, Long>> sums = sumInput.map(new SumReader());
+        DataSet<Tuple3<String, String, Long>> conditionals = conditionalInput.map(new ConditionalReader());
+        DataSet<Tuple2<String, Long>> sums = sumInput.map(new SumReader());
 
-    DataSource<String> testData = env.readTextFile(Config.pathToTestSet());
+        DataSource<String> testData = env.readTextFile(Config.pathToTestSet());
 
-    DataSet<Tuple3<String, String, Double>> classifiedDataPoints = testData.map(new Classifier())
-        .withBroadcastSet(conditionals, "conditionals")
-        .withBroadcastSet(sums, "sums");
+        DataSet<Tuple3<String, String, Double>> classifiedDataPoints = testData.map(new Classifier())
+                .withBroadcastSet(conditionals, "conditionals")
+                .withBroadcastSet(sums, "sums");
 
-    classifiedDataPoints.writeAsCsv(Config.pathToOutput(), "\n", "\t", FileSystem.WriteMode.OVERWRITE);
+        classifiedDataPoints.writeAsCsv(Config.pathToOutput(), "\n", "\t", FileSystem.WriteMode.OVERWRITE);
 
-    env.execute();
-  }
-
-  public static class ConditionalReader implements MapFunction<String, Tuple3<String, String, Long>> {
-
-    @Override
-    public Tuple3<String, String, Long> map(String s) throws Exception {
-      String[] elements = s.split("\t");
-      return new Tuple3<String, String, Long>(elements[0], elements[1], Long.parseLong(elements[2]));
+        env.execute();
     }
-  }
 
-  public static class SumReader implements MapFunction<String, Tuple2<String, Long>> {
+    public static class ConditionalReader implements MapFunction<String, Tuple3<String, String, Long>> {
 
-    @Override
-    public Tuple2<String, Long> map(String s) throws Exception {
-      String[] elements = s.split("\t");
-      return new Tuple2<String, Long>(elements[0], Long.parseLong(elements[1]));
+        @Override
+        public Tuple3<String, String, Long> map(String s) throws Exception {
+            String[] elements = s.split("\t");
+            return new Tuple3<String, String, Long>(elements[0], elements[1], Long.parseLong(elements[2]));
+        }
     }
-  }
+
+    public static class SumReader implements MapFunction<String, Tuple2<String, Long>> {
+
+        @Override
+        public Tuple2<String, Long> map(String s) throws Exception {
+            String[] elements = s.split("\t");
+            return new Tuple2<String, Long>(elements[0], Long.parseLong(elements[1]));
+        }
+    }
 
 
-  public static class Classifier extends RichMapFunction<String, Tuple3<String, String, Double>>  {
+    public static class Classifier extends RichMapFunction<String, Tuple3<String, String, Double>> {
 
-     private final Map<String, Map<String, Long>> wordCounts = Maps.newHashMap();
-     private final Map<String, Long> wordSums = Maps.newHashMap();
+        private final Map<String, Map<String, Long>> wordCounts = Maps.newHashMap();
+        private final Map<String, Long> wordSums = Maps.newHashMap();
 
-     @Override
-     public void open(Configuration parameters) throws Exception {
-       super.open(parameters);
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            super.open(parameters);
 
-       // IMPLEMENT ME
-     }
+            List<Tuple3<String, String, Long>> conditionals = getRuntimeContext().getBroadcastVariable("conditionals");
+            List<Tuple2<String, Long>> sums = getRuntimeContext().getBroadcastVariable("sums");
 
-     @Override
-     public Tuple3<String, String, Double> map(String line) throws Exception {
 
-       String[] tokens = line.split("\t");
-       String label = tokens[0];
-       String[] terms = tokens[1].split(",");
 
-       double maxProbability = Double.NEGATIVE_INFINITY;
-       String predictionLabel = "";
+        }
 
-       // IMPLEMENT ME
+        @Override
+        public Tuple3<String, String, Double> map(String line) throws Exception {
 
-       return new Tuple3<String, String, Double>(label, predictionLabel, maxProbability);
-     }
-   }
+            String[] tokens = line.split("\t");
+            String label = tokens[0];
+            String[] terms = tokens[1].split(",");
+
+            double maxProbability = Double.NEGATIVE_INFINITY;
+            String predictionLabel = "";
+
+            // IMPLEMENT ME
+
+            // label is here the actual label
+            return new Tuple3<String, String, Double>(label, predictionLabel, maxProbability);
+        }
+
+    }
 
 }
